6c6f9bcde40abd307120646d4abed8f580943c01
Anton Lukianenko <antlukyanenko@gmail.com>
2016-10-02 05:17:29 +0000

    Init

---
+++ caesar.c
@@ -0,0 +1,40 @@
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+
+int main(int argc, string argv[])
+{
+    if (argc!=2)
+    {
+        printf("Enter the key\n");
+        return 1;
+    }
+    
+    int k = atoi(argv[1]);
+    
+    string s = GetString();
+    
+    for (int i = 0, n = strlen(s); i < n; i++)
+{
+    if (isalpha (s[i]))
+    {
+     if (isupper (s[i]))
+     {
+        printf("%c", ((((s[i] - 65) +k) %26) +65));
+     }
+     else
+     {
+        printf("%c", ((((s[i] - 97) +k) %26) +97));
+     }
+    }
+    else
+    {
+        printf("%c", s[i]);
+    }
+}
+    printf("\n");
+    return 0;
+}
